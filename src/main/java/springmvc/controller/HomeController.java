a7df54faf7a2da466e21a1639a4a9a4465c598ec
Vaibhav <vaibhavmittal2017@gmail.com>
2024-11-25 14:53:29 +0530

    configured spring mvc basic project

---
+++ src/main/java/springmvc/controller/HomeController.java
@@ -0,0 +1,20 @@
+package springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/home")
+	public String home() {
+		System.out.println("This is home URL");
+		return "index";
+	}
+	
+	@RequestMapping("/about")
+	public String about() {
+		System.out.println("This is about URL");
+		return "about";
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Home Page</title>
+</head>
+<body>
+    <h1>This is home page</h1>
+    <h1>Called by home controller</h1>
+    <h1>URL /home</h1>
+</body>
+</html>
