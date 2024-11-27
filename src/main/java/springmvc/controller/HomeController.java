1902dbbcf6670d1500e3a4d344ce3f2800b1bb5b
Vaibhav <vaibhavmittal2017@gmail.com>
2024-11-27 18:05:10 +0530

    data from controller to view using model

--- src/main/java/springmvc/controller/HomeController.java
+++ src/main/java/springmvc/controller/HomeController.java
@@ -1,14 +1,35 @@
 package springmvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
+//	@RequestMapping("/home")
+//	public String home() {
+//		System.out.println("This is home URL");  //This method sending static data on the UI page
+// 		return "index";
+//	}
+	
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model model) {
 		System.out.println("This is home URL");
+		
+		model.addAttribute("name", "Vaibhav Mittal");  //This method sending dynamic data on the UI page
+		model.addAttribute("id",1234);
+		
+		List<String> friends = new ArrayList<String>();
+		friends.add("Vivek");
+		friends.add("Karan");
+		friends.add("Aayush");
+		
+		model.addAttribute("f",friends);
+		
 		return "index";
 	}
 	
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+    pageEncoding="ISO-8859-1" import="java.util.List, java.util.ArrayList"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,5 +10,28 @@
     <h1>This is home page</h1>
     <h1>Called by home controller</h1>
     <h1>URL /home</h1>
+    
+    <!-- This is a statically entered name -->
+    <h1>Name is: Vaibhav Mittal</h1>
+    
+    <!-- This is a dynamically generated name -->
+    <% 
+      String name = (String) request.getAttribute("name");
+      Integer id = (Integer) request.getAttribute("id");
+      List<String> friends = (List<String>) request.getAttribute("f");
+    %>
+    
+    <h1>Name is: <%=name%></h1>
+    
+    <h1>ID is: <%=id%></h1>
+    
+    <%
+      for(String s:friends){
+    %>
+      <h1><%=s%></h1>
+    <%
+      }
+    %>
+    
 </body>
 </html>
